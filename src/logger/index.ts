ed37bcc77354ad0d28b9e64d41ac1cc22aa7f850
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-03-19 11:13:17 +0100

    refactor(BUX-612): adjust to new prettierrc

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,13 +1,13 @@
-import pino from "pino";
+import pino from 'pino'
 
 const logger = pino({
-    name: 'spv-wallet-js-client',
-    browser: {
-        asObject: true
-    },
-    formatters: {
-        level: (label) => ({level: label})
-    }
+  name: 'spv-wallet-js-client',
+  browser: {
+    asObject: true,
+  },
+  formatters: {
+    level: (label) => ({ level: label }),
+  },
 })
 
-export default logger;
+export default logger
