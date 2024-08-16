2d3e90808cd0e9bc539a6d3d7c9067b5c41d242c
Damian Orzepowski <damian.orzepowski@4chain.studio>
2024-08-16 11:55:03 +0200

    fix(SPV-999): SharedConfig type is not representing server response correctly. (#1069)

---
+++ examples/get-handled-paymail-domains.ts
@@ -0,0 +1,19 @@
+import { SpvWalletClient } from '../dist/typescript-npm-package.cjs.js';
+import { exampleAdminKey, exampleXPriv } from './example-keys.js';
+import { errMessage } from './utils.js';
+
+const server = 'http://localhost:3003/v1';
+
+// It is possible to get paymail domains either as an admin or as a user.
+if (!exampleAdminKey && !exampleXPriv) {
+  console.log(errMessage('adminKey'), 'or', errMessage('xPriv'));
+  process.exit(1);
+}
+
+const client = exampleXPriv
+  ? new SpvWalletClient(server, { xPriv: exampleXPriv })
+  : new SpvWalletClient(server, { adminKey: exampleAdminKey });
+
+const sharedConfig = await client.GetSharedConfig();
+
+console.log(sharedConfig.paymail_domains[0]);
