911022171fd4b2de94f1e19aa999a1acabe4f50f
Nazarii-4chain <nazarii.hrohol@4chain.studio>
2024-09-10 15:32:47 +0300

    chore(SPV-1016): Added webhook examples

---
+++ examples/admin-subscribe-webhook.ts
@@ -0,0 +1,21 @@
+import { SpvWalletClient } from '../dist/typescript-npm-package.cjs.js';
+import { exampleAdminKey } from './example-keys.js';
+import { errMessage } from './utils.js';
+
+const server = 'http://localhost:3003/v1';
+
+if (!exampleAdminKey) {
+  console.log(errMessage('adminKey'));
+  process.exit(1);
+}
+
+const adminClient = new SpvWalletClient(server, {
+  adminKey: exampleAdminKey,
+});
+
+const webhookSuccessfullySubscribed = await adminClient.AdminSubscribeWebhook(
+  'https://example.com',
+  'tokenHeader',
+  'tokenValue',
+);
+console.log('Webhook response:', webhookSuccessfullySubscribed);
