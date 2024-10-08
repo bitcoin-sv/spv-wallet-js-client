2d5c33e550b423807dcf6382386ffa0d5ed336a4
Wojciech Regulski <48433067+wregulski@users.noreply.github.com>
2024-10-08 10:52:19 +0200

    Update src/client.ts
    Co-authored-by: Nazarii-4chain <150656925+Nazarii-4chain@users.noreply.github.com>

--- src/client.ts
+++ src/client.ts
@@ -237,7 +237,7 @@ export class SpvWalletClient {
   }
 
   /**
-   * Admin only: Get a list of all destinations in the system, filtered by conditions, metadata, and queryParams
+   * Admin only: Get a list of all destinations in the system, filtered by conditions, metadata and queryParams
    *
    * @param {DestinationFilter} conditions   Key value object to use to filter the documents
    * @param {Metadata} metadata       Key value object to use to filter the documents by the metadata
