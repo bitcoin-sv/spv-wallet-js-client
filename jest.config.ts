7f98cda2e0121b822b3f7a34279926ea161fbaa8
Damian-4chain <damian.zoltowski@4chain.studio>
2024-10-08 15:15:38 +0200

    test(SPV-1094): change test match pattern

--- jest.config.ts
+++ jest.config.ts
@@ -148,13 +148,10 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/node_modules/', '/regression_tests/', '__tests__/fixtures/'],
+  testPathIgnorePatterns: ['/node_modules/', '/regression_tests/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
